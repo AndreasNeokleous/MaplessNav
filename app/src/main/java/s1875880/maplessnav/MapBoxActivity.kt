738726c6f7b6e7c3063977b9214137334d2bea74
Andreas <andreasneokleous@outlook.com>
2019-04-05 10:50:18 +0100

    Tilequery request

--- app/src/main/java/s1875880/maplessnav/MapBoxActivity.kt
+++ app/src/main/java/s1875880/maplessnav/MapBoxActivity.kt
@@ -3,15 +3,19 @@ package s1875880.maplessnav
 import android.annotation.SuppressLint
 import android.graphics.BitmapFactory
 import android.graphics.Color
-import android.graphics.drawable.BitmapDrawable
+import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.telecom.Call
+import android.util.Log
 import android.widget.Toast
 import com.mapbox.mapboxsdk.Mapbox
 import com.mapbox.mapboxsdk.maps.MapView
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback
 import androidx.annotation.NonNull
 import androidx.core.content.ContextCompat
+import com.google.android.gms.common.api.Response
+import com.mapbox.android.core.location.*
 import com.mapbox.android.core.permissions.PermissionsListener
 import com.mapbox.android.core.permissions.PermissionsManager
 import com.mapbox.geojson.Feature
@@ -30,13 +34,24 @@ import com.mapbox.mapboxsdk.geometry.LatLng
 import com.mapbox.mapboxsdk.style.layers.PropertyFactory.*
 import com.mapbox.mapboxsdk.style.sources.GeoJsonSource
 
-import com.mapbox.api.tilequery.MapboxTilequery;
+import com.mapbox.api.tilequery.MapboxTilequery
+import com.mapbox.mapboxsdk.camera.CameraPosition
+import com.mapbox.mapboxsdk.camera.CameraUpdateFactory
+import java.lang.Exception
+import java.lang.ref.WeakReference
 import javax.security.auth.callback.Callback
 
 
 class MapBoxActivity : AppCompatActivity(), OnMapReadyCallback, PermissionsListener, MapboxMap.OnMapClickListener {
+
     private var permissionsManager: PermissionsManager = PermissionsManager(this)
     private lateinit var mapboxMap: MapboxMap
+    private lateinit var mapView: MapView
+    private val CLICK_CENTER_GEOJSON_SOURCE_ID = "CLICK_CENTER_GEOJSON_SOURCE_ID"
+    private val RESULT_GEOJSON_SOURCE_ID = "RESULT_GEOJSON_SOURCE_ID"
+    private val LAYER_ID = "LAYER_ID"
+    private lateinit var locationEngine: LocationEngine
+    private val callback = MapBoxLocationCallback(this)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -44,68 +59,94 @@ class MapBoxActivity : AppCompatActivity(), OnMapReadyCallback, PermissionsListe
         // called after access token
         setContentView(R.layout.activity_map_box)
 
-      //  mapView = findViewById(R.id.mapView)
-        mapView?.onCreate(savedInstanceState)
-        mapView?.getMapAsync(this)
+        mapView = findViewById(R.id.mapView)
+        mapView.onCreate(savedInstanceState)
+        mapView.getMapAsync(this)
 
 
     }
 
     override fun onMapReady(mapboxMap: MapboxMap) {
         this.mapboxMap = mapboxMap
-        mapboxMap.setStyle(Style.Builder().fromUrl(
-            "mapbox://styles/mapbox/cjerxnqt3cgvp2rmyuxbeqme7")){
-            enableLocationComponent(it)
-           // addClickLayer(it)
-           // addResultLayer(it)
-          //  mapboxMap.addOnMapClickListener(this)
+     //   mapboxMap.setStyle(Style.Builder().fromUrl(
+     //        "mapbox://styles/mapbox/cjerxnqt3cgvp2rmyuxbeqme7")){
+        mapboxMap.setStyle(Style.MAPBOX_STREETS, object:  Style.OnStyleLoaded {
+            override fun onStyleLoaded(style: Style) {
+                addClickLayer(style)
+                addResultLayer(style)
+                enableLocationComponent(style)
+                mapboxMap.addOnMapClickListener(this@MapBoxActivity)
 
-
-        } }
-
-    override fun onMapClick(point: LatLng): Boolean {
-        var style: Style? = mapboxMap.getStyle();
-        if (style != null){
-            var clickLocationSource: GeoJsonSource?  = style.getSourceAs("CLICK_CENTER_GEOJSON_SOURCE_ID");
-            if (clickLocationSource != null) {
-                clickLocationSource.setGeoJson(Feature.fromGeometry(Point.fromLngLat(point.getLongitude(),
-                    point.getLatitude())));
             }
 
-            makeTilequeryApiCall(style, point);
+
+        })
+           //
+
+    //    }
+    }
+
+    override fun onMapClick(point: LatLng): Boolean {
+        var style: Style? = mapboxMap.style
+        if (style != null){
+            var clickLocationSource: GeoJsonSource?  = style.getSourceAs(CLICK_CENTER_GEOJSON_SOURCE_ID )
+            if (clickLocationSource != null) {
+                clickLocationSource.setGeoJson(Feature.fromGeometry(Point.fromLngLat(point.longitude,
+                    point.latitude
+                )))
+            }
+
+            makeTilequeryApiCall(style, point)
             return true
         }
         return false
     }
 
-    private fun makeTilequeryApiCall(style: Style, point: LatLng){
+    fun makeTilequeryApiCall(style: Style, point: LatLng){
         var tilequery: MapboxTilequery = MapboxTilequery.builder()
             .accessToken("pk.eyJ1IjoiYW5kcmVhc25lb2tlbG91cyIsImEiOiJjanR5NXdoNHEwZW9kM3lwbnRobXNxdGFmIn0.LkYWF5avM-_JVB27lS25zg")
             .mapIds("mapbox.mapbox-streets-v8")
-            .query(Point.fromLngLat(point.getLongitude(), point.getLatitude()))
+            .query(Point.fromLngLat(point.longitude, point.latitude))
             .radius(50)
-            .limit(20)
+            .limit(5)
             .geometry("point")
             .dedupe(true)
             .layers("poi_label")
             .build()
 
-    //    tilequery.enqueueCall()
+        tilequery.enqueueCall(object : retrofit2.Callback<FeatureCollection> {
+            override fun onResponse(
+                call: retrofit2.Call<FeatureCollection>,
+                response: retrofit2.Response<FeatureCollection>
+            ) {
+                val resultSource: GeoJsonSource ?= style.getSourceAs(RESULT_GEOJSON_SOURCE_ID)
+                //if (resultSource != null && response.body()?.features() != null){
+                val featureCollection = response.body()?.features()
+                resultSource?.setGeoJson(FeatureCollection.fromFeatures(featureCollection!!))
+
+                //}
+            }
+
+            override fun onFailure(call: retrofit2.Call<FeatureCollection>, t: Throwable) {
+               // Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show()
+            }
+
+        })
     }
 
     private fun addClickLayer(loadedMapStyle: Style){
-        loadedMapStyle.addImage("Click",BitmapFactory.decodeResource(
-            this.getResources(), R.drawable.abc_ab_share_pack_mtrl_alpha))
+        loadedMapStyle.addImage("CLICK-ICON-ID",BitmapFactory.decodeResource(
+            this.resources, R.drawable.red_marker_s))
 
         loadedMapStyle.addSource(
             GeoJsonSource(
-                "CLICK_CENTER_GEOJSON_SOURCE_ID",
+                CLICK_CENTER_GEOJSON_SOURCE_ID ,
                 FeatureCollection.fromFeatures(arrayOf<Feature>())
             )
         )
 
         loadedMapStyle.addLayer(
-            SymbolLayer("click-layer", "CLICK_CENTER_GEOJSON_SOURCE_ID").withProperties(
+            SymbolLayer("click-layer", CLICK_CENTER_GEOJSON_SOURCE_ID).withProperties(
                 iconImage("CLICK-ICON-ID"),
                 iconOffset(arrayOf(0f, -12f)),
                 iconIgnorePlacement(true),
@@ -115,18 +156,18 @@ class MapBoxActivity : AppCompatActivity(), OnMapReadyCallback, PermissionsListe
     }
 
     private fun addResultLayer(loadedMapStyle: Style){
-        loadedMapStyle.addImage("RESULT", BitmapFactory.decodeResource(
-            this.getResources(), R.drawable.abc_btn_colored_material))
+        loadedMapStyle.addImage("RESULT-ICON-ID", BitmapFactory.decodeResource(
+            this.resources, R.drawable.blue_marker_s))
 
         loadedMapStyle.addSource(
             GeoJsonSource(
-                "RESULT_GEOJSON_SOURCE_ID",
+                RESULT_GEOJSON_SOURCE_ID,
                 FeatureCollection.fromFeatures(arrayOf())
             )
         )
 
         loadedMapStyle.addLayer(
-            SymbolLayer("LAYER_ID", "RESULT_GEOJSON_SOURCE_ID").withProperties(
+            SymbolLayer(LAYER_ID, RESULT_GEOJSON_SOURCE_ID).withProperties(
                 iconImage("RESULT-ICON-ID"),
                 iconOffset(arrayOf(0f, -12f)),
                 iconIgnorePlacement(true),
@@ -160,12 +201,35 @@ class MapBoxActivity : AppCompatActivity(), OnMapReadyCallback, PermissionsListe
             locationComponent.cameraMode = CameraMode.TRACKING
             locationComponent.renderMode = RenderMode.COMPASS
 
+            mapboxMap.animateCamera(
+                CameraUpdateFactory
+                    .newCameraPosition(
+                        CameraPosition.Builder()
+                            .zoom(22.0)
+                            .build()
+                    ), 2000
+            )
 
+            initLocationEngine()
         } else{
             permissionsManager = PermissionsManager(this)
             permissionsManager.requestLocationPermissions(this)
 
         }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun initLocationEngine(){
+        locationEngine = LocationEngineProvider.getBestLocationEngine(this)
+
+        var request : LocationEngineRequest ?= LocationEngineRequest.Builder(1000)
+            .setPriority(LocationEngineRequest.PRIORITY_HIGH_ACCURACY)
+            .setMaxWaitTime(5000).build()
+
+        if (request != null) {
+            locationEngine.requestLocationUpdates(request, callback, getMainLooper())
+        }
+        locationEngine.getLastLocation(callback)
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
@@ -187,36 +251,74 @@ class MapBoxActivity : AppCompatActivity(), OnMapReadyCallback, PermissionsListe
 
     public override fun onStart() {
         super.onStart()
-        mapView?.onStart()
+        mapView.onStart()
     }
 
     public override fun onResume() {
         super.onResume()
-        mapView?.onResume()
+        mapView.onResume()
     }
 
     public override fun onPause() {
         super.onPause()
-        mapView?.onPause()
+        mapView.onPause()
     }
 
     public override fun onStop() {
         super.onStop()
-        mapView?.onStop()
+        mapView.onStop()
     }
 
     override fun onLowMemory() {
         super.onLowMemory()
-        mapView?.onLowMemory()
+        mapView.onLowMemory()
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        mapView?.onDestroy()
+        mapView.onDestroy()
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
-        mapView?.onSaveInstanceState(outState)
+        mapView.onSaveInstanceState(outState)
+    }
+
+    private class MapBoxLocationCallback : LocationEngineCallback<LocationEngineResult> {
+        override fun onFailure(exception: Exception) {
+            var activity : MapBoxActivity = activityWeakReference?.get()!!
+
+            if (activity != null) {
+                Toast.makeText(activity, exception.getLocalizedMessage(),
+                    Toast.LENGTH_SHORT).show();
+            }
+        }
+
+        override fun onSuccess(result: LocationEngineResult?) {
+            var activity : MapBoxActivity = activityWeakReference?.get()!!
+            if (activity != null) {
+                var location: Location? = result?.lastLocation
+                if ( location == null ){
+                    return;
+                }
+
+              //  Toast.makeText(activity, "New location" + " " +
+               //     result?.getLastLocation()?.getLatitude().toString() + " " + result?.getLastLocation()?.getLongitude().toString(),
+              //      Toast.LENGTH_SHORT).show()
+                var point : LatLng ?= LatLng(result?.getLastLocation()?.getLatitude()!!, result?.getLastLocation()?.getLongitude()!!)
+                activity.makeTilequeryApiCall(activity.mapboxMap.style!!,point!!)
+
+                if (activity.mapboxMap != null && result?.lastLocation != null){
+                    activity.mapboxMap.locationComponent.forceLocationUpdate(result.lastLocation)
+                }
+            }
+        }
+
+        private var activityWeakReference: WeakReference<MapBoxActivity>?
+
+        constructor(activity: MapBoxActivity)
+        {
+            this.activityWeakReference = WeakReference<MapBoxActivity>(activity)
+        }
     }
 }
