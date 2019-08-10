59982c98499290648172285dbf473a95562a3d6f
Andreas <andreasneokleous@outlook.com>
2019-08-10 14:08:36 +0100

    tidy

--- app/src/main/java/s1875880/maplessnav/Favourite.kt
+++ app/src/main/java/s1875880/maplessnav/Favourite.kt
@@ -4,8 +4,4 @@ package s1875880.maplessnav
  * Created by Andreas Neokleous.
  */
 
-class  Favourite(placeName: String, point: String) {
-    var id: Int = 0
-    var placeName: String ?= placeName
-    var point: String?= point
-}
+class  Favourite(var placeName: String, var point: String)
--- app/src/main/java/s1875880/maplessnav/PoI.kt
+++ app/src/main/java/s1875880/maplessnav/PoI.kt
@@ -3,6 +3,4 @@ package s1875880.maplessnav
 /**
  * Created by Andreas Neokleous.
  */
-class PoI (var distance: Double,var category_en: String, var name: String, var lat: Double, var long: Double) {
-
-}
+class PoI (var distance: Double,var category_en: String, var name: String, var lat: Double, var long: Double)
